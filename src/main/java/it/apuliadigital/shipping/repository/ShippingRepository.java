04b91e03e04cbc485caaf48fa0940af03c2243f7
Manuel <magini15@gmail.com>
2025-06-20 14:28:59 +0200

    repository aggiornato1

--- src/main/java/it/apuliadigital/shipping/repository/ShippingRepository.java
+++ src/main/java/it/apuliadigital/shipping/repository/ShippingRepository.java
@@ -1,10 +1,18 @@
 package it.apuliadigital.shipping.repository;
 
-import org.springframework.stereotype.Repository;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import it.apuliadigital.shipping.enums.EnumStatus;
 import it.apuliadigital.shipping.models.Shipping;
 
 @Repository
 public interface ShippingRepository extends CrudRepository<Shipping, Integer> {
+    List<Shipping> findByStatus(EnumStatus status);
 
+    List<Shipping> findByCourier(String courier);
+
+    List<Shipping> findByDestinationAddressContainingIgnoreCase(String keyword);
 }
