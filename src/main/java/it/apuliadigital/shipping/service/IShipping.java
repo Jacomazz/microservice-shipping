6646f830f452df38810587312c13cc6acf23e0b7
Manuel <magini15@gmail.com>
2025-06-20 14:23:33 +0200

    repository aggiornato

--- src/main/java/it/apuliadigital/shipping/service/IShipping.java
+++ src/main/java/it/apuliadigital/shipping/service/IShipping.java
@@ -5,8 +5,8 @@ import java.util.List;
 import it.apuliadigital.shipping.models.Shipping;
 
 public interface IShipping {
-    public List<Shipping> getAllShippings();
-    public Shipping createShipping(Shipping newShipping);
-    public Shipping getShippingById(int id);
-    public Shipping updateShipping(Shipping updatedShipping);
+     List<Shipping> getAllShippings();
+     Shipping createShipping(Shipping newShipping);
+     Shipping getShippingById(int id);
+     Shipping updateShipping(Shipping updatedShipping);
 }
