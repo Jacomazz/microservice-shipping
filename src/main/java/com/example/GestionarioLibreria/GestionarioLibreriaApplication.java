69d6fdbeb1a62d446f88827a58579c4bca93436b
Jacomazz <71347062+Jacomazz@users.noreply.github.com>
2025-06-20 10:56:37 +0200

    Add files via upload

---
+++ src/main/java/com/example/GestionarioLibreria/GestionarioLibreriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionarioLibreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionarioLibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionarioLibreriaApplication.class, args);
+	}
+
+}
